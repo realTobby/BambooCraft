f386e085b1a592881401e8458ec6566d147ab1fd
realTobby <tkattanek00@gmail.com>
2020-03-04 19:24:37 +0100

    Basic setup
    Basic TCP Listener

---
+++ BambooCraft/BambooCraft/Program.cs
@@ -0,0 +1,33 @@
+﻿using BambooCraft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+
+namespace BambooCraft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentMode = "unknown";
+
+#if DEBUG
+                currentMode = "[DEV]";
+#else
+                 currentMode = "[RELEASE]"; 
+#endif
+            Console.WriteLine("BambooCraft " + currentMode);
+
+            SocketListener bambooCraftServer = new SocketListener();
+            bambooCraftServer.StartListening();
+        }
+    }
+}
